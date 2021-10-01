95f6166641a3d91f60af0aff4f9d7ad56f032a86
HeeyaJoshi <heeya.lanetteam@gmail.com>
2021-10-01 15:47:42 +0530

    last commit

---
+++ round.cpp
@@ -0,0 +1,39 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+// bool haveFactor(int n)
+// {
+//     for(int i =2;i<n;i++)
+//     {
+//         if(n%i == 0)
+//         return false;
+//     }
+//     return true;
+// }
+
+// int sumOfFactors(int n){
+//     int coun = 0;
+//     for(int i = 2;i<n;i++)
+//     {
+//         if (n%i == 0 && i%2 ==1)
+//         {
+//             if(!haveFactor(i))
+//                 coun += i;
+//         }
+//     }
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     int result;
+//     result = sumOfFactors(n);
+//     cout << result;
+//     return 0;
+
+// }
+int main(){
+    cout << "Hello";
+    return 0;
+}
